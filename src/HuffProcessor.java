dd90c1c67f53237438287dbccb93c75738a840f2
Ram Gollapudy <sriram.gollapudy@duke.edu>
2018-12-08 21:01:08 -0600

    compress and decompress

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * Although this class has a history of several years,
  * it is starting from a blank-slate, new and clean implementation
@@ -47,7 +47,105 @@ public class HuffProcessor {
 			if (val == -1) break;
 			out.writeBits(BITS_PER_WORD, val);
 		}
-		out.close();
+		
+	}
+	public int[] readForCounts(BitInputStream in) {
+		int counts[] = new int[ALPH_SIZE + 1];
+		while(true) {
+			int bit = in.readBits(BITS_PER_WORD);
+			if(bit == -1) {
+				break;
+			}
+			counts[bit]++;
+		}
+		counts[PSEUDO_EOF] = 1;
+		return counts;
+	}
+	public HuffNode makeTreeFromCounts(int[] counts) {
+		PriorityQueue<HuffNode>pq = new PriorityQueue<>();
+		for (int k = 0; k < ALPH_SIZE + 1; k++) {
+			if(counts[k] != 0) {
+				pq.add(new HuffNode(k, counts[k], null, null));
+			}
+		}
+		while(pq.size() > 1) {
+			HuffNode left = pq.remove();
+			HuffNode right = pq.remove();
+			HuffNode t = new HuffNode(-1, left.myWeight + right.myWeight, left, right);
+			pq.add(t);
+		}
+		HuffNode root = pq.remove();
+		return root;
+	}
+	public String[] makeCodingsFromTree(HuffNode root) {
+		String[] encodings = new String[ALPH_SIZE + 1];
+		codingHelper(root, "", encodings);
+		return encodings;
+	}
+	public void codingHelper(HuffNode root, String string, String [] encodings) {
+		if (root.myLeft == null && root.myRight == null) {
+			encodings[root.myValue] = string;
+			return;
+		}
+		codingHelper(root.myLeft, string + "0", encodings);
+		codingHelper(root.myLeft, string + "1", encodings);
+	}
+	public void writeHeader(HuffNode root, BitOutputStream out) {
+		if(root.myLeft == null && root.myRight == null){
+			out.writeBits(1, 1);
+			out.writeBits(BITS_PER_WORD + 1, root.myValue);
+			return;
+		}
+		out.writeBits(1, 0);
+		writeHeader(root.myLeft, out);
+		writeHeader(root.myRight, out);
+	}
+	
+	public HuffNode readTreeHeader(BitInputStream in) {
+		int bit = in.readBits(1);
+		if (bit == -1) throw new HuffException("");
+		if(bit == 0) {
+			HuffNode left = readTreeHeader(in);
+			HuffNode right = readTreeHeader(in);
+			return new HuffNode(0,0,left,right);
+		}
+		else {
+			int value = in.readBits(BITS_PER_WORD + 1);
+			return new HuffNode(value, 0, null, null);
+		}
+	}
+	
+	public void writeCompressedBits(String [] encodings, BitInputStream in, BitOutputStream out) {
+		while(true) {
+			int bit = in.readBits(BITS_PER_WORD);
+			if (bit == -1) break;
+			String code = encodings[bit];
+			out.writeBits(code.length(), Integer.parseInt(code, 2));
+		}
+		String code = encodings[PSEUDO_EOF];
+		out.writeBits(code.length(), Integer.parseInt(code, 2));
+	}
+	public void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out){
+		HuffNode current = root;
+		while (true) {
+			int bit = in.readBits(1);
+			if (bit == -1) {
+				throw new HuffException("bad input, no PSEUDO_EOF");
+			}
+			else {
+				if (bit ==0) current = current.myLeft;
+				else current = current.myRight;
+				if (current.myLeft == null && current.myRight == null) {
+					if (current.myValue == PSEUDO_EOF) {
+						break;
+					}
+					else {
+						out.writeBits(BITS_PER_WORD, current.myValue);
+						current = root;
+					}
+				}
+			}
+		}
 	}
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
@@ -59,12 +157,20 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
-
+		int bits = in.readBits(BITS_PER_INT);
+		if(bits != HUFF_TREE) {
+			throw new HuffException("Illegal header starts with" +bits);
+		}
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root, in, out);
+		out.close();
 		while (true){
 			int val = in.readBits(BITS_PER_WORD);
 			if (val == -1) break;
 			out.writeBits(BITS_PER_WORD, val);
 		}
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root, in, out);
 		out.close();
 	}
 }
